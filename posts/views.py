1f1178fb832f5da3c6c9fb9a32c0133ed6de98ef
Armianto Sumitro <armiantos@gmail.com>
2022-02-19 18:58:58 -0700

    Add post form

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.forms import ModelForm
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from posts.models import Post
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['author', 'categories']
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+
+    def form_valid(self, form: PostForm):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
