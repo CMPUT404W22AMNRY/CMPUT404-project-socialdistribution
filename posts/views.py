bbc928e471c4c30d7ef2dca89526491d4a7a4874
Armianto Sumitro <armiantos@gmail.com>
2022-02-19 21:04:22 -0700

    Add categories field

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django import forms
+from django.db import transaction
 from django.forms import ModelForm
-from django.views.generic.edit import CreateView
-from posts.models import Post
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from posts.models import Post, Category
 
 
 class PostForm(ModelForm):
@@ -9,11 +13,21 @@ class PostForm(ModelForm):
         model = Post
         exclude = ['author', 'categories']
 
+    categories = forms.CharField(max_length=256)
 
-class CreatePostView(LoginRequiredMixin, CreateView):
-    model = Post
+
+class CreatePostView(LoginRequiredMixin, FormView):
     form_class = PostForm
+    template_name = 'posts/post.html'
 
-    def form_valid(self, form: PostForm):
+    def form_valid(self, form: PostForm)-> HttpResponse:
+        form.full_clean()
         form.instance.author = self.request.user
-        return super().form_valid(form)
+
+        with transaction.atomic():
+            form.save()
+            for category in form.cleaned_data['categories'].split(','):
+                db_category = Category.objects.get_or_create(category=category)[0]
+                form.instance.categories.add(db_category)
+            form.save()
+        return redirect('/') # TODO: Update this when we have the post page
