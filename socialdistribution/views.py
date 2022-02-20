1d7f0b695a109f4914a46b0674fcea825bb4a8b7
Armianto Sumitro <armiantos@gmail.com>
2022-02-19 19:11:19 -0700

    Merge stream

--- socialdistribution/views.py
+++ socialdistribution/views.py
@@ -5,8 +5,5 @@ from django.shortcuts import redirect, render
 def root(request: HttpRequest) -> HttpResponse:
     if request.user.is_anonymous:
         return redirect('login')
-    return stream(request)
-
-
-def stream(request: HttpRequest) -> HttpResponse:
     return render(request, 'stream.html')
+
