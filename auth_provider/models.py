4e4ebbc8c41eb205e04e83323d2c2a7cc4b2f2e1
Armianto Sumitro <armiantos@gmail.com>
2022-02-21 16:13:51 -0700

    Extend user model

--- auth_provider/models.py
+++ auth_provider/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("use_app", "Can access the current app"),
+        ]
