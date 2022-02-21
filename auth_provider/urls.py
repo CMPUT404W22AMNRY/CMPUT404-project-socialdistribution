2a2281a0ef08bc1bbf13bc4ecd05c8983542c4ab
Armianto Sumitro <armiantos@gmail.com>
2022-02-21 16:08:10 -0700

    Namespace urls

--- auth_provider/urls.py
+++ auth_provider/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView
 
 from .views import SignUpView
 
+app_name = 'auth_provider'
 urlpatterns = [
     path('signup', SignUpView.as_view(), name='signup'),
     path('login', LoginView.as_view(template_name='auth/login.html'), name='login'),
--- socialdistribution/views.py
+++ socialdistribution/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
 from django.shortcuts import redirect
+from django.http import HttpRequest, HttpResponse
 
 
 def root(request: HttpRequest) -> HttpResponse:
     if request.user.is_anonymous:
-        return redirect('login')
+        return redirect(reverse_lazy('auth_provider:login'))
     # TODO: redirect to user's stream page
     return HttpResponse("Main app")
