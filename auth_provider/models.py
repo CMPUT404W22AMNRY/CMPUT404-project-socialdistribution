1b3daca4700f41e099ccb7c09e3411a404785607
Armianto Sumitro <armiantos@gmail.com>
2022-02-21 16:35:05 -0700

    Remove unused permission and add url fields

--- auth_provider/models.py
+++ auth_provider/models.py
@@ -4,8 +4,5 @@ from django.contrib.auth import get_user_model
 
 class Author(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
-
-    class Meta:
-        permissions = [
-            ("use_app", "Can access the current app"),
-        ]
+    github_url = models.CharField(max_length=512, blank=True)
+    profile_image_url = models.CharField(max_length=512, blank=True)
