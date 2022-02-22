22efdf235a9d085a2a2ee85b27d18295b503e980
Armianto Sumitro <armiantos@gmail.com>
2022-02-22 00:27:25 -0700

    Allow edit post fields except author and categories

--- posts/views.py
+++ posts/views.py
@@ -3,20 +3,19 @@ from django.db import transaction
 from django.forms import ModelForm
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from posts.models import Post, Category
 
 
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        exclude = ['author', 'categories']
-
-    categories = forms.CharField(max_length=256)
-
-
 class CreatePostView(LoginRequiredMixin, FormView):
+    class PostForm(ModelForm):
+        class Meta:
+            model = Post
+            exclude = ['author', 'categories']
+
+        categories = forms.CharField(max_length=256)
+
     form_class = PostForm
     template_name = 'posts/create_post.html'
 
@@ -30,3 +29,10 @@ class CreatePostView(LoginRequiredMixin, FormView):
                 form.instance.categories.add(db_category)
             form.save()
         return redirect('/')  # TODO: Update this when we have the post page
+
+
+class EditPostView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'posts/edit_post.html'
+    fields = ('title', 'description', 'content_type', 'visibility', 'content', 'unlisted')
+    success_url = '/'  # TODO: Update this when we have the post page
