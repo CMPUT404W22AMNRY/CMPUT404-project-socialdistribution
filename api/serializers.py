39e75bf86beb6e6314b7aa51c2ab4797242e32d3
Armianto Sumitro <armiantos@gmail.com>
2022-02-25 23:03:07 -0700

    Add more fields to the rest endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'url', 'username']
+        model = get_user_model()
+        fields = ['id', 'url', ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['type'] = 'author'
+        representation['displayName'] = instance.get_full_name()
+        representation['github'] = instance.github_url
+        representation['profileImage'] = instance.profile_image_url
+        return representation
