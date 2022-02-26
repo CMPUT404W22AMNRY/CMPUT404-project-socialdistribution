ee976da702133a2b994681ff1b9a9ca7c096ee05
Armianto Sumitro <armiantos@gmail.com>
2022-02-25 23:03:00 -0700

    Update author viewset to use custom user model

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework.renderers import JSONRenderer
 
@@ -11,7 +11,7 @@ class AuthorViewSet(viewsets.ModelViewSet):
     renderer_classes = [JSONRenderer]
     pagination_class = page_number_pagination_class_factory([('type', 'authors')])
 
-    queryset = User.objects.all().order_by('id')
+    queryset = get_user_model().objects.filter(is_active=True, is_staff=False).order_by('id')
     serializer_class = AuthorSerializer
     permission_classes = [permissions.IsAuthenticated]
     http_method_names = ['get']
