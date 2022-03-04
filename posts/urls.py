82df01c4e9c3888e21555fabd8da2de9a8bc6776
Armianto Sumitro <armiantos@gmail.com>
2022-03-04 11:09:27 -0700

    Add delete post view

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = 'posts'
 urlpatterns = [
     path('<int:pk>/edit', views.EditPostView.as_view(), name='edit'),
+    path('<int:pk>/delete', views.DeletePostView.as_view(), name='delete'),
     path('<int:pk>/comments/new', views.CreateCommentView.as_view(), name='new-comment'),
     path('<int:pk>', views.PostDetailView.as_view(), name='detail'),
     path('new', views.CreatePostView.as_view(), name='new'),
--- posts/views.py
+++ posts/views.py
@@ -3,11 +3,12 @@ from django.db import transaction
 from django.forms import ModelForm
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from posts.models import Post, Category, Comment
+from django.urls import reverse_lazy
 
 
 class PostForm(ModelForm):
@@ -58,6 +59,13 @@ class EditPostView(LoginRequiredMixin, UpdateView):
 
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
+
+
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts:my-posts')
+    template_name = 'posts/delete_post.html'
+    template_name_suffix = ''
 
 
 class CommentForm(ModelForm):
