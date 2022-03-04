1fbfbd43947421f7f604f04ed0b95d5743a33868
pickabo <nhossain@ualberta.ca>
2022-03-03 19:43:01 -0700

    profile page

--- auth_provider/views.py
+++ auth_provider/views.py
@@ -1,5 +1,9 @@
+from re import template
+from django.forms import ModelForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+
+from auth_provider.models import User
 from .forms import SignUpForm
 
 
@@ -7,3 +11,4 @@ class SignUpView(CreateView):
     template_name = 'auth/signup.html'
     form_class = SignUpForm
     success_url = reverse_lazy('auth_provider:login')
+
--- socialdistribution/urls.py
+++ socialdistribution/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,5 +26,6 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('auth_provider.urls')),
+    path('accounts/profile/', views.ProfileView.as_view(), name='user_profile'),
     path('api/v1/', include('api.urls')),
 ]
