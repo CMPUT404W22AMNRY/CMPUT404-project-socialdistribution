15430a279038364ce6f16ad8f80cab921d822373
Armianto Sumitro <armiantos@gmail.com>
2022-03-03 20:12:50 -0700

    Fix unused/redundant css

--- auth_provider/urls.py
+++ auth_provider/urls.py
@@ -5,7 +5,7 @@ from .views import SignUpView, ProfileView
 
 app_name = 'auth_provider'
 urlpatterns = [
-    path('signup', SignUpView.as_view(), name='signup'),
-    path('login', LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('profile', ProfileView.as_view(), name='user_profile'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('profile/', ProfileView.as_view(), name='user_profile'),
 ]
