73c9e747ba7edf73e800eddf1a4f55f5e16f41ff
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-03-10 15:19:48 -0700

    Implement skeleton /authors/{author_id}/posts/

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
+from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import action
 
-from api.serializers import AuthorSerializer
+from api.serializers import AuthorSerializer #, ImageSerializer
 from api.util import page_number_pagination_class_factory
 
 
@@ -15,3 +17,22 @@ class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     permission_classes = [permissions.IsAuthenticated]
     http_method_names = ['get']
+
+    @action(
+        methods=['get'], 
+        detail=True,         
+        url_path='posts',
+        url_name='posts'
+    )
+    def Posts(self, request, pk=None):
+        # TODO: Implement /authors/{AUTHOR_ID}/POSTS/
+        return Response({})
+
+# class ImageViewSet(viewsets.ModelViewSet):
+#     renderer_classes = [JSONRenderer]
+
+#     # queryset = get_user_model().objects.filter(is_active=True, is_staff=False).order_by('id')
+#     serializer_class = ImageSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+#     http_method_names = ['get']
+#
