292bca3df33f629b72e9480e8af0ba04b0c95d8e
Armianto Sumitro <armiantos@gmail.com>
2022-03-10 23:22:36 -0700

    Define server model

--- servers/models.py
+++ servers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+STR_MAX_LENGTH = 512
+
+
+class Server(models.Model):
+    service_address = models.CharField(max_length=STR_MAX_LENGTH)
+    username = models.CharField(max_length=STR_MAX_LENGTH)
+    password = models.CharField(max_length=STR_MAX_LENGTH)
