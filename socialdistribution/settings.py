c372d102884353e1f6ed438b396c3dd3cdd63dfc
Armianto Sumitro <armiantos@gmail.com>
2022-03-19 00:34:41 -0600

    Use django-heroku to configure static files and database

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -140,3 +140,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+import django_heroku
+django_heroku.settings(locals())
