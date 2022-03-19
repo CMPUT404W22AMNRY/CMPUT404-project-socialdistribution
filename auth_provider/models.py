94b6add7b978621ea789e65b842dc5b9d6b4b134
Armianto Sumitro <armiantos@gmail.com>
2022-03-19 16:19:02 -0600

    Add is API user field

--- auth_provider/models.py
+++ auth_provider/models.py
@@ -8,6 +8,7 @@ from .constants import CHAR_FIELD_MAX_LENGTH
 class User(AbstractUser):
     github_url = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH, blank=True)
     profile_image_url = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH, blank=True)
+    is_api_user = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse('auth_provider:profile', kwargs={'pk': self.id})
