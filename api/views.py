b96b89f900094ead40ee96be9f78ea7156851730
Armianto Sumitro <armiantos@gmail.com>
2022-03-20 19:55:57 -0600

    Link serializer to viewset

--- api/views.py
+++ api/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework.renderers import JSONRenderer
 
-from api.serializers import AuthorSerializer
+from api.serializers import AuthorSerializer, PostSerializer
 from api.util import page_number_pagination_class_factory
+from posts.models import Post
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -13,5 +14,14 @@ class AuthorViewSet(viewsets.ModelViewSet):
 
     queryset = get_user_model().objects.filter(is_active=True, is_staff=False).order_by('id')
     serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ['get']
+
+class PostViewSet(viewsets.ModelViewSet):
+    renderer_classes = [JSONRenderer]
+    pagination_class = page_number_pagination_class_factory([('type', 'posts')])
+
+    queryset = Post.objects.all().order_by('-date_published')
+    serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
     http_method_names = ['get']
