13d02190a620eb65f5baa8df4b66a1497ac70df1
Armianto Sumitro <armiantos@gmail.com>
2022-03-20 19:55:42 -0600

    Create post serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from posts.models import Post
+
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -13,4 +15,16 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
         representation['displayName'] = instance.get_full_name()
         representation['github'] = instance.github_url
         representation['profileImage'] = instance.profile_image_url
+        return representation
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    author  = AuthorSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'description', 'content_type', 'content', 'author', 'categories']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['type'] = 'post'
         return representation
