d22af56b4cfe28c1bc1f18586b148c53711c11ef
Armianto Sumitro <armiantos@gmail.com>
2022-03-20 19:55:48 -0600

    Add tests for posts

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,9 @@ import json
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 
+from posts.models import Post
+from posts.tests.constants import POST_DATA
+
 
 class AuthorTests(TestCase):
     def setUp(self) -> None:
@@ -14,6 +17,37 @@ class AuthorTests(TestCase):
         self.assertEqual(res.status_code, 200)
         body = json.loads(res.content.decode('utf-8'))
         self.assertEqual(body['type'], 'authors')
+        self.assertEqual(len(body['items']), 1)
+
+    def test_authors_require_login(self):
+        res = self.client.get('/api/v1/authors/')
+        self.assertEqual(res.status_code, 403)
+
+    def test_create_author(self):
+        self.client.login(username='bob', password='password')
+        res = self.client.post('/api/v1/authors/', {'username': 'alice', 'password': 'password'})
+        self.assertEqual(res.status_code, 405)
+
+
+class PostTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username='bob', password='password')
+        self.post = Post.objects.create(
+            title=POST_DATA['title'],
+            description=POST_DATA['description'],
+            content_type=POST_DATA['content_type'],
+            content=POST_DATA['content'],
+            author_id=self.user.id,
+            unlisted=POST_DATA['unlisted'])
+        self.post.save()
+
+    def test_posts(self):
+        self.client.login(username='bob', password='password')
+        res = self.client.get(f'/api/v1/posts/{self.post.id}')
+        self.assertEqual(res.status_code, 200)
+        body = json.loads(res.content.decode('utf-8'))
+        self.assertEqual(body['type'], 'posts')
         self.assertEqual(len(body['items']), 1)
 
     def test_authors_require_login(self):
