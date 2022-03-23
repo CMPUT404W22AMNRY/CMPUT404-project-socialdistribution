ea06764cb5814dd1f8d092c9861c255fabad2c56
YeeSkywalker <itsyeelin@gmail.com>
2022-03-22 23:03:29 -0600

    Update views.py

--- api/views.py
+++ api/views.py
@@ -66,4 +66,3 @@ class FollowersViewSet(viewsets.ModelViewSet):
             return Follow.objects.filter(followee=self.kwargs['author_pk']).order_by('-created')
         except e:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
