b06168bfc294e828a7f5dc7b79d59b84d619b66f
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-03-24 15:59:54 -0600

    Update Image URL Validator

--- lib/url.py
+++ lib/url.py
@@ -1,6 +1,8 @@
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 
+import requests
+
 import mimetypes
 
 
@@ -14,11 +16,17 @@ def is_url_valid(url: str) -> bool:
     return True
 
 
+# def is_url_valid_image(url: str) -> bool:
+#     return is_url_valid(url) and is_url_image(url)
+
+
+# # inspired by MattoTodd on StackOverflow https://stackoverflow.com/a/10543969
+# def is_url_image(url: str) -> bool:
+#     mimetype = mimetypes.guess_type(url)
+#     return (mimetype and mimetype[0] and mimetype[0].startswith('image'))
+
 def is_url_valid_image(url: str) -> bool:
-    return is_url_valid(url) and is_url_image(url)
-
-
-# inspired by MattoTodd on StackOverflow https://stackoverflow.com/a/10543969
-def is_url_image(url: str) -> bool:
-    mimetype = mimetypes.guess_type(url)
-    return (mimetype and mimetype[0] and mimetype[0].startswith('image'))
+    h = requests.head(url)
+    header = h.headers
+    content_type = header.get('content-type')
+    return content_type.startswith('image')
