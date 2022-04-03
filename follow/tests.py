3223b24ff1fffee3e06d277c6ba05ea7c7cbe79c
YeeSkywalker <itsyeelin@gmail.com>
2022-04-03 17:26:51 -0600

    Testing

--- follow/tests.py
+++ follow/tests.py
@@ -28,7 +28,6 @@ class FriendRequestsViewTests(TestCase):
         Follow.objects.follow_request(from_user=self.alice, to_user=self.bob)
         self.client.login(username='bob', password='password')
         res = self.client.get(reverse('follow:friend_requests'))
-        print(res)
         self.assertContains(res, self.alice.username)
 
     def test_remote_friend_request(self):
@@ -36,7 +35,7 @@ class FriendRequestsViewTests(TestCase):
         RemoteFollow.objects.create(follower_url=url, followee=self.bob)
         self.client.login(username='bob', password='password')
         res = self.client.get(reverse('follow:friend_requests'))
-        print(res)
+        print(res.body)
         self.assertContains(res, url)
 
 
