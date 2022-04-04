9362ba436ba0580d6baea132e0bd581da9686566
YeeSkywalker <itsyeelin@gmail.com>
2022-04-04 11:54:16 -0600

    Update views.py

--- auth_provider/views.py
+++ auth_provider/views.py
@@ -85,6 +85,7 @@ class RemoteProfileView(ServerDetailView):
         context = super().get_context_data(**kwargs)
 
         def remote_action(remote_action_generator: RemoteActionGenerator):
+            print("abcdefg")
             print(self.get_object())
             remote_action = remote_action_generator(self.request.user, self.get_object())
             if remote_action is None:
