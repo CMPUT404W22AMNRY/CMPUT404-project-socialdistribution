6237ae9d21e6e0979a39e03dd4fc89f8516109db
armiantos <43416725+armiantos@users.noreply.github.com>
2022-04-04 01:05:15 -0600

    Implement inbox comments and display comments from remote authors (#165)
    * Define remote comment type
    
    * Create migrations
    
    * Refactor inbox tests to its own file
    
    * Implement comment on local inbox
    
    * Add remote comment model test
    
    * Add remote comment test
    
    * Format source files
    
    * Render remote comments in api without url
    
    * Display remote comments in post detail

---
+++ api/tests/test_inbox.py
@@ -0,0 +1,160 @@
+import json
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from rest_framework import status
+
+from .constants import SAMPLE_REMOTE_AUTHOR
+from .test_api import TEST_PASSWORD, TEST_USERNAME
+from posts.models import Post
+from posts.tests.constants import COMMENT_DATA, POST_DATA
+from posts.tests.constants import POST_DATA
+
+
+class InboxTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username=TEST_USERNAME, password=TEST_PASSWORD)
+
+    def test_get(self):
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        res = self.client.get(f'/api/v1/authors/{self.user.id}/inbox')
+        self.assertEqual(res.status_code, status.HTTP_501_NOT_IMPLEMENTED)
+
+    def test_post(self):
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        res = self.client.post(f'/api/v1/authors/{self.user.id}/inbox')
+        self.assertEqual(res.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_delete(self):
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        res = self.client.delete(f'/api/v1/authors/{self.user.id}/inbox')
+        self.assertEqual(res.status_code, status.HTTP_501_NOT_IMPLEMENTED)
+
+    def test_like(self):
+        post = Post.objects.create(
+            title=POST_DATA['title'],
+            description=POST_DATA['description'],
+            content_type=POST_DATA['content_type'],
+            content=POST_DATA['content'],
+            author_id=self.user.id,
+            unlisted=POST_DATA['unlisted'])
+        post.save()
+
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        author_response = self.client.get(f'/api/v1/authors/{self.user.id}').content
+        post_response = self.client.get(f'/api/v1/authors/{self.user.id}/posts/{post.id}').content
+        post_url = json.loads(post_response).get('id')
+
+        payload = {
+            'type': 'Like',
+            'author': json.loads(author_response),
+            'object': post_url
+        }
+
+        self.assertEqual(len(post.like_set.all()), 0)
+
+        resp = self.client.post(
+            f'/api/v1/authors/{self.user.id}/inbox',
+            json.dumps(payload),
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.get('Content-Type'), 'application/json')
+
+        self.assertEqual(len(post.like_set.all()), 1)
+
+        body = json.loads(resp.content)
+        self.assertEqual(body.get('type'), 'Like')
+
+    def test_remote_like(self):
+        post = Post.objects.create(
+            title=POST_DATA['title'],
+            description=POST_DATA['description'],
+            content_type=POST_DATA['content_type'],
+            content=POST_DATA['content'],
+            author_id=self.user.id,
+            unlisted=POST_DATA['unlisted'])
+        post.save()
+
+        self.assertEqual(len(post.remotelike_set.all()), 0)
+
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        post_response = self.client.get(f'/api/v1/authors/{self.user.id}/posts/{post.id}').content
+        post_url = json.loads(post_response).get('id')
+
+        payload = {
+            'type': 'Like',
+            'author': json.loads(SAMPLE_REMOTE_AUTHOR),
+            'object': post_url
+        }
+
+        resp = self.client.post(
+            f'/api/v1/authors/{self.user.id}/inbox',
+            json.dumps(payload),
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 204)
+
+        self.assertEqual(len(post.remotelike_set.all()), 1)
+
+
+class InboxCommentTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username=TEST_USERNAME, password=TEST_PASSWORD)
+        self.post = Post.objects.create(
+            title=POST_DATA['title'],
+            description=POST_DATA['description'],
+            content_type=POST_DATA['content_type'],
+            content=POST_DATA['content'],
+            author_id=self.user.id,
+            unlisted=POST_DATA['unlisted'])
+        self.post.save()
+
+    def test_comment(self):
+        self.assertEqual(len(self.post.comment_set.all()), 0)
+
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        post_response = self.client.get(f'/api/v1/authors/{self.user.id}/posts/{self.post.id}').content
+        post_url = json.loads(post_response).get('id')
+
+        author_response = self.client.get(f'/api/v1/authors/{self.user.id}/').content
+        author_json = json.loads(author_response)
+
+        payload = {
+            'type': 'comment',
+            'author': author_json,
+            'comment': COMMENT_DATA['comment'],
+            'contentType': COMMENT_DATA['content_type'],
+            'object': post_url
+        }
+
+        resp = self.client.post(
+            f'/api/v1/authors/{self.user.id}/inbox',
+            json.dumps(payload),
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(self.post.comment_set.all()), 1)
+
+    def test_remote_comment(self):
+        self.assertEqual(len(self.post.remotecomment_set.all()), 0)
+
+        self.client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+        post_response = self.client.get(f'/api/v1/authors/{self.user.id}/posts/{self.post.id}').content
+        post_url = json.loads(post_response).get('id')
+
+        author_json = json.loads(SAMPLE_REMOTE_AUTHOR)
+
+        payload = {
+            'type': 'comment',
+            'author': author_json,
+            'comment': COMMENT_DATA['comment'],
+            'contentType': COMMENT_DATA['content_type'],
+            'object': post_url
+        }
+
+        resp = self.client.post(
+            f'/api/v1/authors/{self.user.id}/inbox',
+            json.dumps(payload),
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 204)
+        self.assertEqual(len(self.post.remotecomment_set.all()), 1)
