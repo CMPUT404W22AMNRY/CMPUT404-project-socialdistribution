a5351f55d91af770eff9adc6edc9ba943fd4b0b3
YeeSkywalker <itsyeelin@gmail.com>
2022-04-03 19:27:24 -0600

    Finish tests

--- follow/tests.py
+++ follow/tests.py
@@ -84,6 +84,28 @@ class RemoteFollowmodelTests(TestCase):
         remote_request.accept()
         self.assertEqual(len(RemoteFollow.objects.all()), 1)
         self.assertEqual(len(RemoteRequest.objects.all()), 0)
+    
+    def test_remote_reject_request(self):
+        remote_request = RemoteRequest.objects.create(from_user_url=self.url, to_user=self.bob)
+        self.assertEqual(len(RemoteFollow.objects.all()), 0)
+        remote_request.reject()
+        self.assertEqual(len(RemoteFollow.objects.all()), 0)
+        self.assertEqual(len(RemoteRequest.objects.all()), 0) 
+
+    def test_remote_accept_request(self):
+        remote_request = RemoteRequest.objects.create(from_user_url=self.url, to_user=self.bob)
+        self.assertEqual(len(RemoteFollow.objects.all()), 0)
+        remote_request.accept()
+        self.assertEqual(len(RemoteFollow.objects.all()), 1)
+        self.assertEqual(len(RemoteRequest.objects.all()), 0)
+
+    def test_remote_remove_follower(self):
+        remote_request = RemoteRequest.objects.create(from_user_url=self.url, to_user=self.bob)
+        self.assertEqual(len(RemoteFollow.objects.all()), 0)
+        remote_request.accept()
+        self.assertEqual(len(RemoteFollow.objects.all()), 1)
+        RemoteFollow.objects.get(followee=self.bob, follower_url=self.url).unfollow()
+        self.assertEqual(len(RemoteFollow.objects.all()), 0)     
 
 
 class UsersViewTests(TestCase):
