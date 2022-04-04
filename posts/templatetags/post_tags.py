c2ead98690c04ff5608d66dd7ec04cd9f19c001b
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-04-04 01:20:49 -0600

    Show Markdown in Stream (With No Links)

--- posts/templatetags/post_tags.py
+++ posts/templatetags/post_tags.py
@@ -1,3 +1,4 @@
+import re
 from markdown_it import MarkdownIt
 
 from django import template
@@ -11,3 +12,12 @@ register = template.Library()
 def convert_markdown(value):
     md = MarkdownIt('commonmark')
     return md.render(value)
+
+
+# https://stackoverflow.com/questions/53980097/removing-markup-links-in-text
+@register.filter
+@stringfilter
+def convert_markdown_no_links(value):
+    md = MarkdownIt('commonmark')
+    md.disable('link')
+    return re.sub(r"\[(.+)\]\(.+\)", r"\1", md.render(value))
